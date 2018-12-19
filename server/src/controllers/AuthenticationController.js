9da99a4d77147f72302bb174736767c15a3eb95d
12miechy <12miechy@gmail.com>
2018-12-19 23:38:52 +0900

    "bluebird, bcrypt-nodejs" coding v0.1

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -31,13 +31,16 @@ module.exports = {
 
       if (!user) {
         return res.status(403).send({
-          error: 'The login information was incorrect'
+          error: 'The login account was not found'
         })
       }
 
-      const isPasswordValid = password === user.password
-      console.log(password, user.password)
+      console.log('call isPasswordValid')
+      const isPasswordValid = await user.comparePassword(password)
+      console.log('isPasswordValid:', isPasswordValid)
+      // console.log(password, user.password)
       if (!isPasswordValid) {
+        console.log('The login information was incorrect')
         return res.status(403).send({
           error: 'The login information was incorrect'
         })
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,8 +1,52 @@
-module.exports = (sequelize, DataTypes) =>
-  sequelize.define('User', {
+const Promise = require('bluebird')
+// bcrypt-nodejs内のすべてのメソッドにPromise版を追加する
+const bcrypt = Promise.promisifyAll(require('bcrypt-nodejs'))
+
+function hashPassword (user, options) {
+  const SALT_FACTOR = 8
+  console.log('hashPassword')
+
+  if (!user.changed('password')) {
+    console.log('return')
+    return
+  }
+  console.log('bcrypt')
+  return bcrypt
+    .genSaltAsync(SALT_FACTOR)
+    .then(salt => bcrypt.hashAsync(user.password, salt, null))
+    .then(hash => {
+      user.setDataValue('password', hash)
+    })
+}
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
     email: {
       type: DataTypes.STRING,
       unique: true
     },
     password: DataTypes.STRING
+  }, {
+    hooks: {
+      beforeCreate: hashPassword,
+      beforeUpdate: hashPassword,
+      beforeSave: hashPassword
+    }
   })
+
+  User.prototype.comparePassword = function (password) {
+    console.log('comparePassword')
+    console.log(password, this.password)
+    console.log('compareAsync')
+    console.log(bcrypt.compareAsync(password, this.password))
+    console.log('compareSync')
+    console.log(bcrypt.compareSync(password, this.password))
+    return bcrypt.compareAsync(password, this.password)
+  }
+
+  User.associate = function (models) {
+  }
+
+  console.log('User')
+  return User
+}
