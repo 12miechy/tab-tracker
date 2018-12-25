0c548e0947769a0308dcc8e55317a36a4805764f
12miechy <12miechy@gmail.com>
2018-12-25 23:05:19 +0900

    Created "CreateSong" screen v0.1

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -3,7 +3,7 @@
     <v-flex xs6 offset-xs3>
       <panel title="Songs">
         <div v-for="song in songs"
-          :key="song.title">
+          :key="song.id">
           {{ song.title }} -
           {{ song.artist }} -
           {{ song.album }}
@@ -26,7 +26,7 @@ export default {
     }
   },
   async mounted () {
-    this.songs = await SongsService.index()
+    this.songs = (await SongsService.index()).data
   }
 }
 </script>
