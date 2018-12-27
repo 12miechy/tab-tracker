25ba685b347984cac187934c11fa3a7ce29217b3
12miechy <12miechy@gmail.com>
2018-12-27 22:53:00 +0900

    Coding "CreateSong" logic

--- client/src/components/CreateSong.vue
+++ client/src/components/CreateSong.vue
@@ -1,64 +1,93 @@
 <template>
-  <div>
-    <panel title="Song Metadata">
-      <v-text-field
-        label="Title"
-        v-model="title"
-      ></v-text-field>
+  <v-layout>
+    <v-flex xs4>
+      <panel title="Song Metadata">
+        <v-text-field
+          label="Title"
+          v-model="song.title"
+        ></v-text-field>
 
-      <v-text-field
-        label="Artist"
-        v-model="artist"
-      ></v-text-field>
+        <v-text-field
+          label="Artist"
+          v-model="song.artist"
+        ></v-text-field>
 
-      <v-text-field
-        label="Genre"
-        v-model="genre"
-      ></v-text-field>
+        <v-text-field
+          label="Genre"
+          v-model="song.genre"
+        ></v-text-field>
 
-      <v-text-field
-        label="Album"
-        v-model="album"
-      ></v-text-field>
+        <v-text-field
+          label="Album"
+          v-model="song.album"
+        ></v-text-field>
 
-      <v-text-field
-        label="Album Image Url"
-        v-model="albumImageUrl"
-      ></v-text-field>
+        <v-text-field
+          label="Album Image Url"
+          v-model="song.albumImageUrl"
+        ></v-text-field>
 
-      <v-text-field
-        label="Youtube Id"
-        v-model="youtubeId"
-      ></v-text-field>
+        <v-text-field
+          label="Youtube Id"
+          v-model="song.youtubeId"
+        ></v-text-field>
 
-      <v-text-field
-        label="Lyrics"
-        v-model="lyrics"
-      ></v-text-field>
+      </panel>
+    </v-flex>
+    <v-flex xs8>
+      <panel title="Song Structure" class="ml-2">
+        <v-text-field
+          label="Tab"
+          multi-line
+          v-model="song.tab"
+        ></v-text-field>
 
-      <v-text-field
-        label="Tab"
-        v-model="tab"
-      ></v-text-field>
+        <v-text-field
+          label="Lyrics"
+          multi-line
+          v-model="song.lyrics"
+        ></v-text-field>
+      </panel>
 
-    </panel>
-  </div>
+      <v-btn
+        dark
+        class="cyan"
+        @click="create">
+        Create Song
+      </v-btn>
+     </v-flex>
+  </v-layout>
 </template>
 
 <script>
 import Panel from '@/components/Panel'
+import SongsService from '@/services/SongsService'
 
 export default {
   data () {
     return {
-      title: null,
-      artist: null,
-      genre: null,
-      album: null,
-      albumImageUrl: null,
-      youtubeId: null,
-      lyrics: null,
-      tab: null
+      song: {
+        title: null,
+        artist: null,
+        genre: null,
+        album: null,
+        albumImageUrl: null,
+        youtubeId: null,
+        lyrics: null,
+        tab: null
+      }
+    }
+  },
+  methods: {
+    async create () {
+      try {
+        SongsService.post(this.song)
+        this.$router.push({
+          name: 'songs'
+        })
+      } catch (err) {
+        console.log(err)
+      }
     }
   },
   components: {
