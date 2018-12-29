7bcc45809fde531b4409bb9cdd35b837a829f6f9
12miechy <12miechy@gmail.com>
2018-12-29 20:00:29 +0900

    Create ViewSong.vue v0.1

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -15,11 +15,39 @@
           <v-icon>add</v-icon>
         </v-btn>
 
-        <div v-for="song in songs"
+        <div
+          v-for="song in songs"
+          class="song"
           :key="song.id">
-          {{ song.title }} -
-          {{ song.artist }} -
-          {{ song.album }}
+
+          <v-layout>
+            <v-flex xs6>
+              <div class="song-title">
+                {{song.title}}
+              </div>
+              <div class="song-artist">
+                {{song.artist}}
+              </div>
+              <div class="song-genre">
+                {{song.genre}}
+              </div>
+              <v-btn
+                dark
+                class="cyan"
+                @click="navigeteTo({
+                  name: 'song',
+                  params: {
+                    songId: song.id
+                  }
+                })">
+                View
+              </v-btn>
+
+            </v-flex>
+            <v-flex xs6>
+              <img class="album-image" :src="song.albumImageUrl" />
+            </v-flex>
+          </v-layout>
         </div>
       </panel>
     </v-flex>
@@ -50,4 +78,26 @@ export default {
 </script>
 
 <style scoped>
+.song {
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
+
+.song-title {
+  font-size: 30px;
+}
+
+.song-artist {
+  font-size: 24px;
+}
+
+.song-genre {
+  font-size: 18x;
+}
+
+.album-image {
+  width: 70%;
+  margin: 0 auto;
+}
 </style>
